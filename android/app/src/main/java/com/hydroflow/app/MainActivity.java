cc2b44d44219a78d0c15d6aace3d33ed4778458e
SudharsanDeepak <sudharsandeepak12@gmail.com>
2025-11-02 09:59:36 +0530

    APP Developed

--- android/app/src/main/java/com/hydroflow/app/MainActivity.java
+++ android/app/src/main/java/com/hydroflow/app/MainActivity.java
@@ -1,5 +1,25 @@
 package com.hydroflow.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+// App Center imports
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize Microsoft App Center
+        AppCenter.start(
+                getApplication(),
+                "4d8a6dbf-fdfc-449f-9765-6c32858245e1",
+                Analytics.class,
+                Crashes.class
+        );
+    }
+}
